7871ec781bb8fc8678eaf7339b25b82abe9b0e9e
inga.isom.kwlv <inga.isom.kwlv@statefarm.com>
2024-06-27 17:15:03 -0400

    add test file

---
+++ src/test/java/com/galvanize/LightSaberTest.java
@@ -0,0 +1,32 @@
+package com.galvanize;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LightSaberTest {
+
+    @Test
+    public void isSerialNumberAvaliable() {
+        LightSaber lightSaber = new LightSaber();
+        assertEquals
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
